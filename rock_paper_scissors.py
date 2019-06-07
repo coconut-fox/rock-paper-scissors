e4b668af86aab7c0cc090ad1df6fb5f91b5424f2
coconut-fox <51126442+coconut-fox@users.noreply.github.com>
2019-06-07 17:59:38 +0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,3 @@
-import sys
 print('Hello! Do you want to play Rock-Paper-Scissors?(type yes or no)')
 answer = input()
 if answer == 'no':
@@ -9,51 +8,41 @@ if answer == 'yes':
     if answer2 == 'player':
         print('Player 1, what do you pick?(type rock, paper or scissors)')
         player1 = input()
-        if player1 != 'rock' or 'paper' or 'scissors':
-            print('Invalid input.')
-            sys.exit
+        win = 1
         print('Player 2, what do you pick?(type rock, paper or scissors)')
         player2 = input()
-        if player2 != 'rock' or 'paper' or 'scissors':
-            print('Invalid input.')
-            sys.exit
-        elif player1 == player2:
+        if player1 == player2:
             print('Draw!')
         elif player1 == 'rock':
-            if player2 == 'scissors':
-                print('Player 1 wins!')
-            elif player2 == 'paper':
-                print('Player 2 wins!')
-        elif player1 == 'paper':
-            if player2 == 'rock':
-                print('Player 1 wins!')
-            elif player2 == 'scissors':
-                print('Player 2 wins!')
-        elif player1 == 'scissors':
             if player2 == 'paper':
-                print('Player 1 wins!')
-            elif player2 == 'rock':
-                print('Player 2 wins!')
-    else:
-        import random
-        choices = ['rock','paper','scissors']
-        comp = random.choice(choices)
-        print('Player, what do you pick?(type rock, paper or scissors)')
-        play = input()
-        if comp == play:
-            print('It\'s a draw!')
-        elif comp == 'rock':
-            if play == 'scissors':
-                print('The computer has won!(rock vs scissors)')
-            elif play == 'paper':
-                print('The player has won!(rock vs paper)')
-        elif comp == 'paper':
-            if play == 'rock':
-                print('The computer has won!(paper vs rock)')
-            elif play == 'scissors':
-                print('The player has won!(paper vs scissors)')
-        elif comp == 'scissors':
-            if play == 'paper':
-                print('The computer has won!(scissors vs paper)')
-            elif play == 'rock':
-                print('The player has won!(scissors vs rock)')
+                win = 2
+        elif player1 == 'paper':
+            if player2 == 'scissors':
+                win = 2
+        elif player1 == 'scissors':
+            if player2 == 'rock':
+                win = 2
+    print('Player' + str(win) + ' wins!')
+else:
+    import random
+    choices = ['rock', 'paper', 'scissors']
+    comp = random.choice(choices)
+    print('Player, what do you pick?(type rock, paper or scissors)')
+    play = input()
+    if comp == play:
+        print('It\'s a draw!')
+    elif comp == 'rock':
+        if play == 'scissors':
+            print('The computer has won!(rock vs scissors)')
+        elif play == 'paper':
+            print('The player has won!(rock vs paper)')
+    elif comp == 'paper':
+        if play == 'rock':
+            print('The computer has won!(paper vs rock)')
+        elif play == 'scissors':
+            print('The player has won!(paper vs scissors)')
+    elif comp == 'scissors':
+        if play == 'paper':
+            print('The computer has won!(scissors vs paper)')
+        elif play == 'rock':
+            print('The player has won!(scissors vs rock)')
